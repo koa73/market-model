ae2149be5333beb2ad8061b0c5e65b1dfceda90f
MixaSg <msgibnev@gmail.com>
2018-12-14 23:09:20 +0300

    LSTM version

--- lstmdataman.py
+++ lstmdataman.py
@@ -10,12 +10,15 @@ def loaddata(filename, separator):
 
 def prepadedata(main_ticker_data, train_seq, train_vol):
     # --- Разделяем данные на учебный и проверочный наборы
+    input_train = []
+    output_train = []
 
     # Получаем размерность массива
     data_row = main_ticker_data.shape[0]
     data_column = main_ticker_data.shape[1]
     # Переводим в формат np.array
     main_ticker_data = main_ticker_data.values
+    print("main_ticker_data.shape: ", main_ticker_data.shape)
     # Тренировочные данные начинаются с 0 элемента массива
     train_start = 0
     # до 80% всего набора данных. На выходе получаем целое число от train_vol*data_row
@@ -25,9 +28,11 @@ def prepadedata(main_ticker_data, train_seq, train_vol):
     test_end = data_row
     data_train = main_ticker_data[np.arange(train_start, train_end), :]
     data_test = main_ticker_data[np.arange(test_start, test_end), :]
+
     # --- Нормализация учебного набора. Тестовый набор должен нормализоваться этими же параметрами mean и std
     data_train, data_mean, data_std = normax(data_train)
     data_test = normay(data_test, data_mean, data_std)
+
     # --- Делаем реверс наборов, для того, чтобы избежать удаления последних данных при целочисленном делении
     reverse_order = []
     for i in range(0, data_train.shape[0]):
@@ -39,32 +44,27 @@ def prepadedata(main_ticker_data, train_seq, train_vol):
     #data_test_reverse = data_test[reverse_order.astype(np.int64)]
 
     # --- Подготовка учебного набора
-    # Делим на последовательности "Кол-во измерений, кол-во шагов, кол-во элементов в измерении"
-    # Находим количество строк, на сколько будет разбит новый массив
-    print("data_train_reverse.shape[0]: ", data_train_reverse.shape[0])
-    row_train = data_train_reverse.shape[0] // train_seq
+    print("data_train_reverse.shape: ", data_train_reverse.shape)
+    # Количество наборов в массиве с учетом смещения при комбинаторике
+    row_train = (data_train_reverse.shape[0] // (train_seq + 1)) - (train_seq + 1)
     print("row_train: ", row_train)
-    # Обрезаем массив для y
-    data_train_reverse_y = data_train_reverse[:(row_train * train_seq)]
-    data_train_reverse_y = data_train_reverse_y[:, 1:4]
-    print("data_train_reverse_y.shape", data_train_reverse_y.shape)
-    # Обрезаем массив для X. Смещаем на 1 строку, так как нам нужно иметь y_train для последнего набора данных и вычитаем последний набор для сохранения размерности
-    data_train_reverse_x = data_train_reverse[1:(row_train * train_seq) - train_seq + 1]
-    print("data_train_reverse_x.shape", data_train_reverse_x.shape)
-    # Трансформируем массив
-    X_train = data_train_reverse_x.reshape((row_train - 1), data_train_reverse_x.shape[1], train_seq)
-    print("X_train.shape", X_train.shape)
-    # y_train должен быть значением колонок <HIGH>;<LOW>;<CLOSE>. Последовательность 0, train_seq + 1 и т.д. с шагом train_seq
-    print("row_train:", row_train)
-    # Берем первую строку (предсказание для первого набора), колонки <HIGH>;<LOW>;<CLOSE>
-    y_train = data_train_reverse[0, 1:4]
-    #print("data_train_reverse_y: ", data_train_reverse_y)
-    print("y_train:", y_train)
-    for i in range(1, (row_train - 1)):
-        #x = np.array(data_train_reverse_y.iloc[i: i + seq_len, 1])
-        #print(data_train_reverse_y[i * (train_seq + 1), 1:3])
-        y_train = np.append(y_train, data_train_reverse_y[i * (train_seq + 1), 1:4])
-    print("y_train.shape: ", y_train.shape)
+    print("data_train_reverse[0]: ", data_train_reverse[0])
+    #exit(0)
+    for i in range(0, 2):
+    #for i in range(0, row_train):
+        x = np.array(data_train_reverse[i: i + train_seq])
+        #print("x: ", x)
+        y = np.array(data_train_reverse[i + train_seq + 1, 1:4])
+        #print("y: ", y)
+        input_train.append(x)
+        output_train.append(y)
+    X_train = np.array(input_train)
+    y_train = np.array(output_train)
+
+    print(X_train.shape)
+    print(y_train.shape)
+    exit(0)
+
     # --- Подготовка тестового набора
 
     # --- Обратный реверс
