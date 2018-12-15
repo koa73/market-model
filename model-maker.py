9b7c9c465d9451517682f820580f3c40364cab54
koa73 <koa73@bk.ru>
2018-12-15 21:47:51 +0300

    -- home work

--- model-maker.py
+++ model-maker.py
@@ -12,7 +12,7 @@ model = tf.keras.Sequential()
 model.add(tf.keras.layers.Dense(135, input_shape=(X_train.shape[1],), activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(123, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(116, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(4))
+model.add(tf.keras.layers.Dense(4, activation=tf.nn.relu))
 
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 model.fit(X_train, y_train, epochs=38, batch_size=10, validation_split=0.05, verbose=2)      #Тренировка сети
@@ -21,12 +21,12 @@ model.fit(X_train, y_train, epochs=38, batch_size=10, validation_split=0.05, ver
 # Сохраняем сеть
 data.save(model)
 
-#Проверка обучкемя
+# Проверка обучения
 X_validate, y_validate = data.get_validation_data()
 score = model.evaluate(X_validate, y_validate, verbose=0)
 print("Точность работы на проверочных данных : %.2f%%" % (score[1]*100))
 
-#Тестирование модели
+# Тестирование модели
 X_test, y_test = data.get_test_data()
 score = model.evaluate(X_test, y_test, verbose=0)
 print("Точность работы на тестовых данных : %.2f%%" % (score[1]*100))
---
+++ model_verify.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import dataman_new as d
+
+model = "last_1"
+
+# Загрузка проверочных данных
+data = d.DataManager("USDRUB", 5, 2)
+X_test, y_test = data.get_test_data()
+
+#print(data.de_norma(X_test))
+
+# Загружаем сеть
+json_file = open(data.get_current_dir()+"/models/"+model+".json", "r")
+loaded_model_json = json_file.read()
+json_file.close()
+
+loaded_model = tf.keras.models.model_from_json(loaded_model_json)                   # Создаем модель
+loaded_model.load_weights(data.get_current_dir()+"/models/"+model+".h5")            # Загружаем веса
+loaded_model.compile(loss='mse', optimizer='adam', metrics=['mae'])                 # Компилируем
+
+mse, mae = loaded_model.evaluate(X_test, y_test)            #Проверка на тестовых данных, определяем величину ошибок
+print("MSE  %f" % mse)
+print("MAE  %f" % mae)
+
+print("=====================================")
+predict = data.denorm_x_array(loaded_model.predict(X_test))    # Предсказания
+y_test_denorm = data.denorm_y_array(y_test)
+
+for i in range(len(y_test)):
+    print(predict[i], y_test_denorm[i], "\t", [y_test_denorm[i][0]-predict[i][0], predict[i][1]-y_test_denorm[i][1]])
