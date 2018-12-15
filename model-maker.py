27cb574dcc057de21088f4e2c0d4cb70039a57ac
koa73 <koa73@bk.ru>
2018-12-15 17:16:00 +0300

    -- home work

--- model-maker.py
+++ model-maker.py
@@ -9,10 +9,10 @@ X_train, y_train = data.get_edu_data()
 print(data.get_variations_num())
 
 model = tf.keras.Sequential()
-model.add(LSTM(256, input_shape=(X_train.shape[1], X_train.shape[2]), activation=tf.nn.sigmoid))
+model.add(tf.keras.layers.LSTM(256, input_shape=(X_train.shape[1], X_train.shape[2]), activation=tf.nn.relu))
 #model.add(tf.keras.layers.LSTM(128, activation=tf.nn.sigmoid, recurrent_dropout=0.2, dropout=0.2))
-model.add(Dense(128, activation=tf.nn.sigmoid))
-model.add(Dense(2))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(2))
 
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 model.fit(X_train, y_train, epochs=5, batch_size=10)      #Тренировка сети
--- test_verify.py
+++ test_verify.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.6
 import dataman_new as d
 
 
