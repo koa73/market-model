804401e4e3e28f82380941c6d697a9a854832ac2
MixaSg <msgibnev@gmail.com>
2018-12-20 08:37:39 +0300

    Make LSTM great again

--- LSTM-test/lstm-maker.py
+++ LSTM-test/lstm-maker.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 ticker = 'SBER'
 market_identifier = 'TQBR'
 start_date = '2010-01-01'
-end_date = '2018-12-03'
+end_date = '2018-12-10'
 separator = ','
 
 
@@ -20,7 +20,7 @@ separator = ','
 main_ticker_data = lstmdataman.loaddata(ticker, separator)
 
 train_vol = 0.9         # Сколько берем от объема для обучения
-train_seq = 2           # Непрерывная последовательность, для которой будем искать предсказание: Х дня -> 1 ответ
+train_seq = 1           # Непрерывная последовательность, для которой будем искать предсказание: Х дня -> 1 ответ
 batch_size = 1
 epochs = 5
 
@@ -40,19 +40,19 @@ model.add(tf.keras.layers.Dense(3))
 """
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.LSTM(512, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True))
-model.add(tf.keras.layers.LSTM(512, activation='relu', dropout=0.2, return_sequences=True))
 model.add(tf.keras.layers.LSTM(256, activation='relu', return_sequences=True))
-model.add(tf.keras.layers.LSTM(256, activation='relu', return_sequences=True))
-model.add(tf.keras.layers.LSTM(256, activation='relu', return_sequences=True))
-model.add(tf.keras.layers.LSTM(64, activation='relu', dropout=0.2))
+model.add(tf.keras.layers.LSTM(128, activation='relu', return_sequences=True))
+model.add(tf.keras.layers.LSTM(64, activation='relu'))
 #model.add(tf.keras.layers.Dense(3))
 model.add(tf.keras.layers.Dense(1))
 
 
-model.compile(loss='mse', optimizer='RMSprop', metrics=['mae'])
+#model.compile(loss='mse', optimizer='RMSprop', metrics=['mae'])
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 
 print("\n====== Train ======\n")
-model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)      #Тренировка сети
+#model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.01, verbose=1)      #Тренировка сети
+model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1)      #Тренировка сети
 
 print("\n====== Test ======\n")
 mse, mae = model.evaluate(X_test, y_test) #Проверка на тестовых данных, определяем величину ошибок
@@ -63,20 +63,7 @@ pred = model.predict(X_test)    # Предсказания
 
 p_input_test = []
 y_output_test = []
-"""
-#Денормализация
-for i in range(0, len(y_test) - 1):
-    last_pred = pred[i]
-    last_y = y_test[i]
-    last_pred *= data_std[1:4]          #Умножаем на стандартное отклонение для 0 столбца
-    last_pred += data_mean[1:4]         #Прибавляем среднее для 0 столбца
-    last_y *= data_std[1:4]
-    last_y += data_mean[1:4]
-    # Вывод результатов прогона по тестовому массиву - первый, середина и последний
-    print(last_pred, last_y, (last_pred - last_y))
-    p_input_test.append(last_pred)
-    y_output_test.append(last_y)
-"""
+
 #Денормализация
 for i in range(0, len(y_test) - 1):
     last_pred = pred[i]
@@ -106,7 +93,7 @@ ax1.set(xlabel='time (Day)', ylabel='Close (RUB)', title='Cost')
 ax1.legend()
 
 #line1, = ax1.plot(y_test_plot[np.arange(0, len(y_test) - 1), 0], label="Tst_HI")  #Отображаем на графике тестовые данные по колонке 0 по y_test (HIGH)
-line1, = ax1.plot(y_test_plot[np.arange(0, len(y_test) - 1)], label="Tst_HI")  #Отображаем на графике тестовые данные по колонке 0 по y_test (HIGH)
+line1, = ax1.plot(y_test_plot[np.arange(0, len(y_test) - 1)], label="Tst_Clo")  #Отображаем на графике тестовые данные по колонке 0 по y_test (HIGH)
 #line2, = ax1.plot(pred_test_plot[np.arange(0, len(y_test) - 1), 0], label="Prd_HI")  # Предсказания по колонке 0 оранжевым цветом
 line2, = ax1.plot(pred_test_plot[np.arange(0, len(y_test) - 1)], label="Prd_Clo")  # Предсказания по колонке 0 оранжевым цветом
 
