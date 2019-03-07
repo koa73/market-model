f87b1787fe082c06322fc26b5c3d9737d199a109
OAKutsenko <Ghtlfnjh12>
2019-03-07 15:08:42 +0300

    --

--- dataprepare.py
+++ dataprepare.py
@@ -16,6 +16,9 @@ class DataPrepare:
     # Количество столбцов данных выбираемых из файла
     __data_col = 4
 
+    __mean = 0
+    __std = 0
+
     __full_data = []
 
     def __init__(self, tiker, batch_size, control_size):
@@ -76,16 +79,16 @@ class DataPrepare:
         x_array = []
         y_array = []
 
-        n_array = self.__norma(np.array(self.__full_data[start:end], dtype="float64"))
+        n_array = np.array(self.__full_data[start:end], dtype="float64")
         data_len = n_array.shape[0]
 
         for i in range(0, data_len-self.__batch_size+1):
             end = self.__batch_size-self.__control_size
             n_array_slice = n_array[i:i + self.__batch_size]
-            x_array.append(np.concatenate(n_array_slice[:end], axis=None))
+            x_array.append(np.concatenate(self.__norma_x(n_array_slice[:end]), axis=None))
             # Удаление лишних данных (<OPEN> High Low <CLOSE><VAL>)
             y_array.append(
-                np.concatenate((np.delete(n_array_slice[end:], np.s_[0, 3, 4], 1)),
+                np.concatenate((np.delete(self.__norma_y(n_array_slice[end:]), np.s_[0, 3, 4], 1)),
                                axis=None))
 
         return np.array(x_array,  dtype="float64"),  np.array(y_array,  dtype="float64")
@@ -98,18 +101,27 @@ class DataPrepare:
         return np.reshape(np.expand_dims(data, axis=1), (data.shape[0], int(data[0].shape[0]/self.__data_col),
                                                          self.__data_col))
 
-    def __norma(self, data):
+    def __norma_x(self, data):
         """
         :param data: Массив данных
         :return: Нормализованный массив data_return, среднее по столбцу data_mean, стандартное отклонение по столбцу data_std
         """
-        data_std = data.std(axis=0, dtype=np.float64)  # Определяем стандартное отклонение по каждому столбцу
-        data_mean = data.mean(axis=0, dtype=np.float64)  # Вычисляем среднее по каждому столбцу
+        self.__std = data.std(axis=0, dtype=np.float64)  # Определяем стандартное отклонение по каждому столбцу
+        self.__mean = data.mean(axis=0, dtype=np.float64)  # Вычисляем среднее по каждому столбцу
         data_return = data.astype(np.float64)  # Приведение типов
-        data_return -= data_mean  # Вычитаем среднее
-        data_return /= data_std  # Делим на отклонение
+        data_return -= self.__mean  # Вычитаем среднее
+        data_return /= self.__std  # Делим на отклонение
         return data_return
 
+    def __norma_y(self, data):
+        """
+        :param data: Массив данных
+        :return: Нормализованный массив data_return, среднее по столбцу mean, стандартное отклонение по столбцу std
+        """
+        data_return = data.astype(np.float64)  # Приведение типов
+        data_return -= self.__mean  # Вычитаем среднее
+        data_return /= self.__std  # Делим на отклонение
+        return data_return
 
 
     """
--- new-var/test.py
+++ new-var/test.py
@@ -4,5 +4,7 @@ data = d.DataPrepare("USDRUB", 5, 1)
 
 X_edu, Y_edu = data.get_edu_data(True)
 
+print(X_edu[0])
+exit(0)
 for i in range(len(X_edu)):
     print(X_edu[i])
