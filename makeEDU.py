42b32db6c4610f19d25e24fefa00dc25debc8b43
OAKutsenko <Ghtlfnjh12>
2020-07-21 16:15:46 +0300

    --

--- makeEDU.py
+++ makeEDU.py
@@ -2,7 +2,7 @@
 import dataMiner as d
 
 
-data = d.DataMiner(3)
+data = d.DataMiner(3, '0.001')
 data.make_edu_data()
 
 
--- prepare.py
+++ prepare.py
@@ -2,7 +2,7 @@
 import dataMiner as d
 
 
-data = d.DataMiner(3)
+data = d.DataMiner(3, '0.1')
 data.prepare_data()
 
 
