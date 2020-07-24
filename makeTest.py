56a336bb66f24dea5532b5a79c18213307a520a3
OAKutsenko <Ghtlfnjh12>
2020-07-24 20:04:16 +0300

    --

--- makeTest.py
+++ makeTest.py
@@ -3,6 +3,6 @@ import dataMiner as d
 
 
 data = d.DataMiner(3)
-tikers = data.make_test_case_prepare('test/','test/rawdata/', 'MSFT')
+tikers = data.make_test_case_prepare('test/','test/rawdata/', 'TSLA')
 #print(tikers)
-data.make_test_data(tikers, '31.01.2020', 6)
+data.make_test_data(tikers, '24.06.2020', 6)
