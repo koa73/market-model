3e46e899242c1365810a705aa1daecefa81a5d0c
OAKutsenko <Ghtlfnjh12>
2020-07-27 16:46:15 +0300

    --

--- makeTest_BIBARY.py
+++ makeTest_BIBARY.py
@@ -2,5 +2,5 @@
 import dataMiner as d
 
 data = d.DataMiner(3)
-tikers = data.make_test_case_prepare('test/','test/rawdata/', ['AAPL', 'MSFT'])
+tikers = data.make_test_case_prepare('test/','test/rawdata/', ['AAPL', 'MSFT', 'TSLA'])
 data.male_test_data_binary(tikers)
