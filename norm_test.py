1f7aa9e1c9f6357bdd179da3394b5d703f7bd53c
OAKutsenko <Ghtlfnjh12>
2020-08-03 17:00:00 +0300

    --

--- norm_test.py
+++ norm_test.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from keras.layers import Input, Dense, Dropout, Concatenate, BatchNormalization
 
 
-data =  tf.constant(np.array([[7,4,2,6],[1,0,12,10],[2,5,5,2]]))
+data =  tf.constant(np.array([[7,4,2,6],[1,0,12,10],[2,5,5,2]]), dtype=tf.float32)
 layer = tf.keras.layers.LayerNormalization(axis=1)
 output = layer(data)
 print(output)
