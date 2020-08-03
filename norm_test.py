4304b74d03d084af07a4fd51bb5e2f0cf4861554
OAKutsenko <Ghtlfnjh12>
2020-08-03 16:46:46 +0300

    --

--- norm_test.py
+++ norm_test.py
@@ -1,10 +1,12 @@
-import keras
+#!/usr/bin/env python3
+
+
 import numpy as np
 import tensorflow as tf
 
 
 data = np.array([[7,4,2],[1,0,12],[5,5,2]])
 print(data)
-layer = tf.keras.layers.LayerNormalization(axis=1)
-output = layer(data)
-print(output)
+#layer = tf.keras.layers.LayerNormalization(axis=1)
+#output = layer(data)
+#print(output)
