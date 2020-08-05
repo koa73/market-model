8879ccb2f208d82f38435c7aa8827daf1df52be7
OAKutsenko <Ghtlfnjh12>
2020-08-05 12:53:18 +0300

    -- One day change --1

--- makeEDU.py
+++ makeEDU.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 import dataMiner as d
+import sys
 
+
+if (len(sys.argv) < 2):
+    print("Argument not found ")
+    exit(0)
 
 data = d.DataMiner(3)
 # 1- long list
 # 2 - short list
-data.make_edu_data(2, '_last_b11')
+data.make_edu_data(2, '_last_'+sys.argv[1])
 
 
