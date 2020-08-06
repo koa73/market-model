133f4f6508d9c2fe04486200ac97e6c64737d713
OAKutsenko <Ghtlfnjh12>
2020-08-06 19:52:52 +0300

    -- New prepare format --

--- make_data.py
+++ make_data.py
@@ -9,4 +9,4 @@ if (len(sys.argv) < 2):
 
 data = d.DataMaker()
 #data.get_Xy_arrays('edu', 0, '')
-data.get_Xy_arrays(str(sys.argv[1]), int(sys.argv[2]))
+data.get_Xy_arrays(str(sys.argv[1]), int(sys.argv[2]), '')
