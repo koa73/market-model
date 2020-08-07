0c103f3d0475a64d63032164f8e316d042431c4f
OAKutsenko <Ghtlfnjh12>
2020-08-07 16:49:51 +0300

    -- 07.08.2020 ----

--- model_maker.py
+++ model_maker.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-from keras.layers import Input, Dense, Dropout, Concatenate
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
-from keras.models import Model
+import tensorflow as tf
 import sys
 import dataMiner as D
 
@@ -21,38 +19,39 @@ y_train =  data.get_edu('edu_y_', sys.argv[1])
 print("X_edu : " + str(X_train.shape))
 print("y_edu : " + str(y_train.shape))
 
-print(X_train[0])
-#input("Wait any key ....")
-# This returns a tensor
-inputs = Input(shape=(24,) )
+input_layer_1 = tf.keras.layers.Input(shape=(X_train.shape[1], X_train.shape[2]))
+norma_layer = tf.keras.layers.LayerNormalization(axis=1)(input_layer_1)
+hidden_d2_dense = tf.keras.layers.Dense(48, activation='tanh')(norma_layer)
+hidden_d3_dense = tf.keras.layers.Dense(96, activation='tanh')(hidden_d2_dense)
+hidden_d4_dense = tf.keras.layers.Dense(24, activation='tanh')(hidden_d3_dense)
+hidden_d5_dense = tf.keras.layers.Dense(6, activation='tanh')(hidden_d4_dense)
+output = tf.keras.layers.Dense(3, activation='softmax')(hidden_d5_dense)
 
-# a layer instance is callable on a tensor, and returns a tensor
-x = Dense(48, activation='tanh')(inputs)
-x = Dense(96, activation='tanh')(x)
-x = Dense(24, activation='tanh')(x)
-x = Dense(6, activation='tanh')(x)
-predictions = Dense(3,  activation='softmax', name="output")(x)
+#
+model = tf.keras.models.Model(inputs=[input_layer_1], outputs=[output])
+#
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#
+print(model.summary())
 
-# This creates a model that includes
-# the Input layer and three Dense layers
-model = Model(inputs=inputs, outputs=predictions)
 data.save_conf(model, sys.argv[1])                                                  # Запись конфигурации скти для прерывания расчета
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['acc'])
 
-'''
-saves the model weights after each epoch if the validation loss decreased
-'''
-checkpointer = ModelCheckpoint(filepath = data.get_current_dir()+ "/data/model_test/weights_"+sys.argv[1]+".h5", verbose=1, save_best_only=True)
 
-'''
-уменьшать значение шага градиентного спуска
-'''
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=10, min_lr=0.000001, verbose=1)
+dirPath = data.get_current_dir()+ "/data/model_test/"
+# Сохранение модели с лучшими параметрами
+checkpointer = tf.keras.callbacks.ModelCheckpoint(monitor='accuracy',
+                                                  filepath = dirPath + "weights_"+sys.argv[1]+".h5",
+                                                  verbose=1, save_best_only=True)
+# Уменьшение коэфф. обучения при отсутствии изменения ошибки в течении learn_count эпох
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='accuracy', factor=0.1, patience=5, min_lr=0.000001,
+                                                 verbose=1)
+# Остановка при переобучении. patience - сколько эпох мы ждем прежде чем прерваться.
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=5, verbose=0, mode='auto')
 
-early_stopping = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=5, verbose=1, mode='auto')
-
-model.fit(X_train, y_train, epochs=100, batch_size=10, validation_split=0.05, verbose=1, callbacks=[checkpointer,
-                                                                                                    reduce_lr,
-                                                                                                    early_stopping])  # starts training
+# Тренировка сети
+model.fit(X_train, y_train, validation_split=0.05, epochs=100,
+          batch_size=10, verbose=1, shuffle=True,
+          callbacks=[checkpointer, reduce_lr, early_stopping])
