9826f5b97982a1563a508f8ef5bbd8d1bcfd4659
OAKutsenko <Ghtlfnjh12>
2020-08-27 18:48:15 +0300

    -- 07.08.2020 ----

--- test_l.py
+++ test_l.py
@@ -3,7 +3,7 @@ import ShaperLayer as c
 
 x = tf.constant([[1., 2.], [3., 4.]])
 print(x.numpy())
-my_sum = c.ComputeSum(2)
+my_sum = c.ShaperLayer(2)
 y = my_sum(x)
 print(y.numpy())
 y = my_sum(x)
