bd1c6161e08c316ec71722e3732c74870c4bd1b6
OAKutsenko <Ghtlfnjh12>
2020-08-31 19:07:58 +0300

    -- 07.08.2020 ----

--- ShaperLayer.py
+++ ShaperLayer.py
@@ -32,6 +32,7 @@ class ShaperLayer(tf.keras.layers.Layer):
 
     def call(self, inputs):
         vector = tf.unstack(inputs)
+        print (vector[0])
         result = tf.convert_to_tensor(self.concat_result(vector.numpy()))
         return tf.stack(vector)
 
