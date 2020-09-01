4f6c34fad86988286758f82e93f29824c990c7d0
OAKutsenko <Ghtlfnjh12>
2020-09-01 17:29:52 +0300

    -- 07.08.2020 ----

--- SimpleConcatLayer.py
+++ SimpleConcatLayer.py
@@ -39,9 +39,8 @@ class SimpleConcatLayer(tf.keras.layers.Layer):
 
     def __init__(self,input_dim):
         super(SimpleConcatLayer, self).__init__()
-        self.total = tf.Variable(initial_value=tf.zeros((input_dim,)), trainable=False, dtype='float32')
+        #self.total = tf.Variable(initial_value=tf.zeros((input_dim,)), trainable=False, dtype='float32')
 
     def call(self, inputs):
-        input(input.shape)
         return tf.convert_to_tensor(self.__concat_result(inputs.numpy()))
 
