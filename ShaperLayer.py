455ded9cb541fcde938eeb678ee8c5effe515ec8
OAKutsenko <Ghtlfnjh12>
2020-09-01 16:52:38 +0300

    -- 07.08.2020 ----

--- ShaperLayer.py
+++ ShaperLayer.py
@@ -8,7 +8,7 @@ class ShaperLayer(tf.keras.layers.Layer):
         convert_dict = {0: 1, 1: 0, 2: -1}
 
         winner = np.argwhere(vector == np.amax(vector))
-        print(type(winner))
+        print(winner)
         if (winner.size > 1):
             return 0
         else:
