1d4add307aba4ce4b7c72351de8933a661a3d041
OAKutsenko <Ghtlfnjh12>
2020-09-01 13:30:01 +0300

    -- 07.08.2020 ----

--- test_l.py
+++ test_l.py
@@ -1,9 +1,31 @@
 import tensorflow as tf
 import ShaperLayer as c
+import numpy as np
+import csv
+import os
 
-x = tf.constant([0.58, 0.14, 0.28, 0.33, 0.15, 0.52, 0.16, 0.06, 0.78])
-my_sum = c.ShaperLayer(2)
-y = my_sum(x)
-print(y.numpy())
-y = my_sum(x)
-print(y.numpy())
+
+__fileDir = os.path.dirname(os.path.abspath(__file__))
+inputDir = __fileDir + '/data/test/tmp/'
+filename = inputDir + 'UP.csv'
+
+
+with open(filename, newline='') as f:
+    rows = csv.reader(f, delimiter=';', quotechar='|')
+    i=0
+    for row in rows:
+
+        x = tf.constant(list(np.float_(row[0:9])))
+        my_sum = c.ShaperLayer(2)
+        y = my_sum(x)
+        input(y.numpy())
+    f.close()
+
+
+
+#x = tf.constant([0.58, 0.14, 0.28, 0.33, 0.15, 0.52, 0.16, 0.06, 0.78])
+#
+#
+#
+#y = my_sum(x)
+#print(y.numpy())
