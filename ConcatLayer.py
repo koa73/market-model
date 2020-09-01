0630bf4f42c003b5824f272ea2af632ed548ac25
OAKutsenko <Ghtlfnjh12>
2020-09-01 19:04:25 +0300

    -- 07.08.2020 ----

--- ConcatLayer.py
+++ ConcatLayer.py
@@ -11,6 +11,19 @@ class ConcatLayer(tf.keras.layers.Layer):
         else:
             return self.convert_dict[winner[0][0]]
 
+    def __find_best_data(self, up, none, down, idx):
+
+        tmp = np. array([up[idx], none[idx], down[idx]], dtype='float32')
+        max_index_array = np.argmax(tmp, axis=0)
+        if(max_index_array == 1):
+            return none
+        elif (max_index_array == 0):
+            return up
+        else:
+            return down
+
+
+
     def __concat_result(self, __array):
 
         vector_up = np.array(__array).astype(np.float32)[0:3]
@@ -23,19 +36,10 @@ class ConcatLayer(tf.keras.layers.Layer):
 
         calc_value = abs(max_index_none) * (max_index_up + max_index_down )
 
-        if (calc_value == 0):
-            return vector_none
-        elif (calc_value >= 1):
-            return 1
-            #return np.array([1, 0, 0])
-        elif (calc_value <= -1):
-            return -1
-            #return np.array([0, 0, 1])
-
-        #return  calc_value
+        return self.__find_best_data(vector_up,vector_none,vector_down, calc_value)
 
     def __init__(self, input_dim):
-        super(ShaperLayer, self).__init__()
+        super(ConcatLayer, self).__init__()
         self.convert_dict = {0: 1, 1: 0, 2: -1}
 
     def call(self, inputs):
