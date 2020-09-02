abe4981dee5a90c83b2958bf3b4772ddd3c19c3e
OAKutsenko <Ghtlfnjh12>
2020-09-02 13:07:07 +0300

    -- 02.09.2020 ----

--- test_l_2.py
+++ test_l_2.py
@@ -4,6 +4,15 @@ import numpy as np
 import csv
 import os
 
+convert_dict = {0: 1, 1: 0, 2: -1}
+
+def get_max_index(vector):
+
+    winner = np.argwhere(vector == np.amax(vector))
+    if (winner.size > 1):
+        return 0
+    else:
+        return convert_dict[winner[0][0]]
 
 __fileDir = os.path.dirname(os.path.abspath(__file__))
 inputDir = __fileDir + '/data/test/tmp/'
@@ -21,5 +30,7 @@ with open(filename, newline='') as f:
         x = tf.constant(list(np.float_(row[0:9])), dtype='float32')
         my_sum = c.ConcatLayer()
         y = my_sum(x)
-        input(y.numpy())
+        calc_val = get_max_index(y)
+        input (calc_val)
     f.close()
+
