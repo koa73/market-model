150dce4ecaaed903ab9512253d4b768c8e4e6f6a
OAKutsenko <Ghtlfnjh12>
2020-09-02 19:48:53 +0300

    -- 02.09.2020 ----

--- ConcatLayer.py
+++ ConcatLayer.py
@@ -68,5 +68,5 @@ class ConcatLayer(tf.keras.layers.Layer):
 
         if (tf.executing_eagerly() == False):
             tf.compat.v1.executing_eagerly()
-        return tf.convert_to_tensor(self.__concat_result(np.array(inputs)))
+        return tf.convert_to_tensor(self.__concat_result(inputs.numpy()))
 
