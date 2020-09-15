a60cc111a6fae0cd397742170d4a0707988fbef2
OAKutsenko <Ghtlfnjh12>
2020-09-15 16:51:27 +0300

    -- 04.09.2020 ----

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ data = d.DataMaker()
 inputDir = data.get_file_dir()+'/data/'
 
 print(" -----> "+ inputDir + str(sys.argv[2]))
-array = np.load(input+str(sys.argv[2]))
+array = np.load(inputDir + str(sys.argv[2]))
 print (" <<<< "+array.shape[0])
 for i in range(array.shape[0]):
     input(array[i])
