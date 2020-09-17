05e5be791b3ac3cfd69f28e68dcc55b4c43f1951
OAKutsenko <Ghtlfnjh12>
2020-09-17 16:43:00 +0300

    -- 17.09.2020 ----

--- model_maker.py
+++ model_maker.py
@@ -20,9 +20,10 @@ X_NONE = np.load(workDir + 'edu_X_NONE_'+ sys.argv[1]+ '.npy')
 y_UP = np.load(workDir + 'edu_y_UP_'+ sys.argv[1]+ '.npy')
 y_DOWN = np.load(workDir + 'edu_y_DOWN_'+ sys.argv[1]+ '.npy')
 y_NONE = np.load(workDir + 'edu_y_NONE_'+ sys.argv[1]+ '.npy')
+class_weight = {0: 1., 1: 3.5, 2: 1.}
 
 X_train = np.concatenate((X_DOWN,X_UP), axis=0)
-y_train = np.concatenate((y_NONE,y_UP), axis=0)
+y_train = np.concatenate((y_DOWN,y_UP), axis=0)
 X_train = np.concatenate((X_train,X_NONE), axis=0)
 y_train = np.concatenate((y_train,y_NONE), axis=0)
 print ("Shape X: " + str(X_train.shape))
@@ -67,5 +68,5 @@ reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='accuracy', factor=0.1,
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=0, mode='auto')
 
 # Тренировка сети
-model.fit(X_train, y_train, validation_split=0.05, epochs=100,
+model.fit(X_train, y_train, class_weight=class_weight, validation_split=0.05, epochs=100,
 batch_size=10, verbose=1, shuffle=True,callbacks=[checkpointer, reduce_lr, early_stopping])
