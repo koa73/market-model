01f7980f13e8e47a7d33bee5fa7a51ca9c26b4df
OAKutsenko <Ghtlfnjh12>
2020-09-17 14:50:14 +0300

    -- 17.09.2020 ----

--- checker_1.py
+++ checker_1.py
@@ -67,7 +67,7 @@ y_up_pred_test = model.predict([X_up])
 y_none_pred_test = model.predict([X_none])
 y_down_pred_test = model.predict([X_down])
 
-data.check_single_model(y_up_pred_test, y_none_pred_test, y_down_pred_test, prefix)
+data.check_single_model(y_up_pred_test, y_none_pred_test, y_down_pred_test, sys.argv[1])
 
 y_pred_test = np.zeros(shape=(y_up.shape[0], 9))     # Сюда положим результаты прогона X_up моделями up, none, down
 
