ee9531ffa1c1ae1cc70d092364c0f87f2daec1d7
OAKutsenko <Ghtlfnjh12>
2020-09-20 10:40:44 +0300

    -- 17.09.2020 ----

--- composit_model_maker.py
+++ composit_model_maker.py
@@ -2,6 +2,7 @@
 
 import tensorflow as tf
 import sys
+import numpy as np
 import modelMaker as d
 import ConcatLayer as c
 
@@ -35,4 +36,19 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 print(model.summary())
 data.save_conf(model,'composite')                                                  # Запись конфигурации скти для прерывания расчета
 
-#model = tf.keras.models.Model(inputs=[input_layer_1], outputs=[output])
+model.fit(X_down, y_down, validation_split=0.05, epochs=100,batch_size=10, verbose=1)
+
+# ===================== Make prediction =====================
+y_up_pred_test = model.predict([X_up])
+y_none_pred_test = model.predict([X_none])
+y_down_pred_test = model.predict([X_down])
+
+y_pred_test = np.zeros(shape=(y_up.shape[0], 9))     # Сюда положим результаты прогона X_up моделями up, none, down
+
+for i in range(0, y_up_pred_test.shape[0]):
+    y_pred_test[i] = [y_up_pred_test[i, 0], y_up_pred_test[i, 1], y_up_pred_test[i, 2],
+                      y_none_pred_test[i, 0], y_none_pred_test[i, 1], y_none_pred_test[i, 2],
+                      y_down_pred_test[i, 0], y_down_pred_test[i, 1], y_down_pred_test[i, 2]]
+
+print("====== Save predicted data ======\n")
+np.savetxt(data.get_file_dir() + '\complex_1.csv', y_pred_test, delimiter=';')
