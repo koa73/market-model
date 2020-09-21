d9206e0dac32b937aadf327a59bb8c41bc6928f4
OAKutsenko <Ghtlfnjh12>
2020-09-22 00:17:08 +0300

    -- 17.09.2020 ----

--- ConcatLayer.py
+++ ConcatLayer.py
@@ -7,7 +7,8 @@ class ConcatLayer(tf.keras.layers.Layer):
     def __get_max_index(self, vector):
 
         winner = tf.where(vector == tf.math.reduce_max(vector))
-        if(winner.shape[0] > 1):
+        #input(winner)
+        if(winner.shape[0] - 1):
             return 0
         else:
             return tf.math.argmax(tf.reverse(vector, [0]))-1
