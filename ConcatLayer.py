f32498f822b4f890551350706b5cf8ba8548431d
OAKutsenko <Ghtlfnjh12>
2020-09-22 19:21:57 +0300

    -- 17.09.2020 ----

--- ConcatLayer.py
+++ ConcatLayer.py
@@ -54,7 +54,7 @@ class ConcatLayer(tf.keras.layers.Layer):
         #print(tf.shape(inputs))
         #print (inputs)
         print(tf.range(0, inputs.shape[0]))
-        input(tf.slice(inputs, [tf.range(0, inputs.shape[0]), 0], [1, -1]))
+        input(tf.slice(inputs, [0, 0], [1, -1]))
         for i in tf.range(0, inputs.shape[0]):
             self.total = tf.concat([self.total, tf.reshape(self.concat_result(inputs[i]), [1, 3])], 0)
         return self.total
