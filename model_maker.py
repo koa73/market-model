706779f0c1c608ddf68a5cb1e369872f799740a3
OAKutsenko <Ghtlfnjh12>
2020-09-24 22:51:33 +0300

    -- 17.09.2020 ----

--- model_maker.py
+++ model_maker.py
@@ -10,6 +10,8 @@ import time
 
 data = d.ModelMaker()
 
+dirPath = data.get_file_dir()+ "/data/model_test/"
+
 print("Start model making ....")
 
 if (len(sys.argv) < 2):
@@ -23,38 +25,43 @@ X_none, y_none = data.get_edu_data('edu','NONE_'+sys.argv[1], '2D')
 class_weight = {0: 1., 1: 1., 2: 1.}
 
 X_train = np.concatenate((X_down, X_up), axis=0)
-y_train = np.concatenate((y_down, y_up), axis=0)
+y_train = np.concatenate((y_none, y_up), axis=0)
 X_train = np.concatenate((X_train, X_none), axis=0)
 y_train = np.concatenate((y_train, y_none), axis=0)
 
-sec = str(time.time())
-input_layer_1 = tf.keras.layers.Input(shape=(24,), name='1'+sec)
-norma_layer = tf.keras.layers.LayerNormalization(axis=1, name='2'+sec)(input_layer_1)
-hidden_d2_dense = tf.keras.layers.Dense(12, activation='tanh', name='3'+sec)(norma_layer)
-hidden_d3_dense = tf.keras.layers.Dense(34, activation='tanh', name='4'+sec)(hidden_d2_dense)
-hidden_d4_dense = tf.keras.layers.Dense(34, activation='tanh', name='5'+sec)(hidden_d3_dense)
-hidden_d5_dense = tf.keras.layers.Dense(6, activation='tanh', name='6'+sec)(hidden_d4_dense)
-output = tf.keras.layers.Dense(3, activation='softmax', name='7'+sec)(hidden_d5_dense)
 
-#
-model = tf.keras.models.Model(inputs=[input_layer_1], outputs=[output])
-#
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#
-print(model.summary())
-data.save_conf(model, sys.argv[1])                                                  # Запись конфигурации скти для прерывания расчета
+def prepare_model():
 
-dirPath = data.get_file_dir()+ "/data/model_test/"
+    sec = str(time.time())
+    input_layer_1 = tf.keras.layers.Input(shape=(24,), name='1' + sec)
+    norma_layer = tf.keras.layers.LayerNormalization(axis=1, name='2' + sec)(input_layer_1)
+    hidden_d2_dense = tf.keras.layers.Dense(12, activation='tanh', name='3' + sec)(norma_layer)
+    hidden_d3_dense = tf.keras.layers.Dense(34, activation='tanh', name='4' + sec)(hidden_d2_dense)
+    hidden_d4_dense = tf.keras.layers.Dense(34, activation='tanh', name='5' + sec)(hidden_d3_dense)
+    hidden_d5_dense = tf.keras.layers.Dense(6, activation='tanh', name='6' + sec)(hidden_d4_dense)
+    output = tf.keras.layers.Dense(3, activation='softmax', name='7' + sec)(hidden_d5_dense)
 
-# Сохранение модели с лучшими параметрами
-checkpointer = tf.keras.callbacks.ModelCheckpoint(monitor='accuracy',
-                                                  filepath = dirPath + "weights_"+sys.argv[1]+".h5",
-                                                  verbose=1, save_best_only=True)
-# Уменьшение коэфф. обучения при отсутствии изменения ошибки в течении learn_count эпох
-reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='accuracy', factor=0.1, patience=5, min_lr=0.000001,
-                                                 verbose=1)
-# Остановка при переобучении. patience - сколько эпох мы ждем прежде чем прерваться.
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=0, mode='auto')
+    #
+    model = tf.keras.models.Model(inputs=[input_layer_1], outputs=[output])
+    #
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #
+
+    data.save_conf(model, sys.argv[1])  # Запись конфигурации скти для прерывания расчета
+    # Сохранение модели с лучшими параметрами
+    checkpointer = tf.keras.callbacks.ModelCheckpoint(monitor='accuracy',
+                                                      filepath=dirPath + "weights_" + sys.argv[1] + ".h5",
+                                                      verbose=1, save_best_only=True)
+    # Уменьшение коэфф. обучения при отсутствии изменения ошибки в течении learn_count эпох
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='accuracy', factor=0.1, patience=5, min_lr=0.000001,
+                                                     verbose=1)
+    # Остановка при переобучении. patience - сколько эпох мы ждем прежде чем прерваться.
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=0,
+                                                      mode='auto')
+    model.fit(X_train, y_train, class_weight=class_weight, validation_split=0.05, epochs=100,
+              batch_size=10, verbose=1, shuffle=True, callbacks=[checkpointer, reduce_lr, early_stopping])
+
+    return model
 
 
 def seq(start, end, step):
@@ -69,8 +76,9 @@ for i in seq(1, 10, 0.1):
     print ("----------------  Start new loop with value : "+ str(i))
     # Тренировка сети
     class_weight[0] = i
-    model.fit(X_train, y_train, class_weight=class_weight, validation_split=0.05, epochs=100,
-              batch_size=10, verbose=1, shuffle=True, callbacks=[checkpointer, reduce_lr, early_stopping])
+    model = prepare_model()
+    if (i == 1):
+        print(model.summary())
 
     # ===================== Data load =========================
 
