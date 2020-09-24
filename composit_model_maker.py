f1b483141f52d0a2ec129c08a5fb26bac2cf72c6
OAKutsenko <Ghtlfnjh12>
2020-09-24 13:26:05 +0300

    -- 17.09.2020 ----

--- composit_model_maker.py
+++ composit_model_maker.py
@@ -21,9 +21,9 @@ X_up, y_up = data.get_check_data('test', 'UP_b38', '2D')
 X_none, y_none = data.get_check_data('test', 'NONE_b38', '2D')
 
 # ====================== Load static models =====================
-model_up = data.model_loader('weights_b25_150_62.0')
+model_up = data.model_loader('weights_b25_150_63.0')
 model_none = data.model_loader('weights_b25_150_3.0')
-model_down = data.model_loader('weights_b25_150_63.0')
+model_down = data.model_loader('weights_b25_150_62.0')
 
 # ===================== Build model =============================
 
