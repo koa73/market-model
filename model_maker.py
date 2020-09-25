b8b6e95ebda6b204cfca5acfdb35c4fa550f7e9f
OAKutsenko <Ghtlfnjh12>
2020-09-25 18:26:57 +0300

    -- 17.09.2020 ----

--- model_maker.py
+++ model_maker.py
@@ -75,7 +75,7 @@ def seq(start, end, step):
     return itertools.islice(itertools.count(start, step), sample_count)
 
 
-for i in seq(0, 1.3, 0.1):
+for i in seq(0.5, 0.9, 0.05):
 
     print ("----------------  Start new loop with value : "+ str(i))
     # Тренировка сети
