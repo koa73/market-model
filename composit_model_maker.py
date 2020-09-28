675dcd9d6a732ce50106a28d752acb401a809979
OAKutsenko <Ghtlfnjh12>
2020-09-28 18:36:49 +0300

    -- 17.09.2020 ----

--- composit_model_maker.py
+++ composit_model_maker.py
@@ -36,7 +36,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 print(model.summary())
 data.save_conf(model,'composite_4')                                                  # Запись конфигурации скти для прерывания расчета
 
-#model.fit([X_up, X_none, X_down], y_down, validation_split=0.05, epochs=2, batch_size=10, verbose=1)
+model.fit([X_up, X_none, X_down], y_down, validation_split=0.05, epochs=2, batch_size=10, verbose=1)
 model.save(data.get_file_dir() + "/data/model_test/weights_composite_4.h5")
 
 # ===================== Make prediction =====================
