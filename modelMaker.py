b7e8bb9104e49ac382c3dc506946b224be4930c9
OAKutsenko <Ghtlfnjh12>
2020-10-08 13:46:33 +0300

    -- 29.09.2020 ----

--- modelMaker.py
+++ modelMaker.py
@@ -146,11 +146,11 @@ class ModelMaker:
             file_count = len([name for name in os.listdir(outputDir + 'models')
                               if os.path.isfile(os.path.join(outputDir + 'models', name))]) / 2
 
-            copyfile(self.__fileDir+ "/data/model_test/"+model_name+".json",
-                 outputDir+"models/"+model_name + "_"+str(int(file_count))+".json" )
+            #copyfile(self.__fileDir+ "/data/model_test/"+model_name+".json",
+            #     outputDir+"models/"+model_name + "_"+str(int(file_count))+".json" )
 
-            copyfile(self.__fileDir + "/data/model_test/" + model_name + ".h5",
-                 outputDir + "models/" + model_name + "_" + str(int(file_count)) + ".h5")
+            #copyfile(self.__fileDir + "/data/model_test/" + model_name + ".h5",
+            #     outputDir + "models/" + model_name + "_" + str(int(file_count)) + ".h5")
 
             with open(filename, append_write, newline='') as csv_out_file:
                 output = csv.writer(csv_out_file, delimiter=';')
