f011f1f5cde7de4225c8bd42dca4a237f8d21935
OAKutsenko <Ghtlfnjh12>
2020-10-09 17:14:01 +0300

    -- 29.09.2020 ----

--- composit_model_maker_loop.py
+++ composit_model_maker_loop.py
@@ -51,15 +51,15 @@ i = 10
      #   for down in range(63, 120):
 
 
-for x in range (len(x1)):
+#for x in range (len(x1)):
     # ====================== Load static models =====================
-    print('>>>>>> Check models UP: ' + str(x1[x]) + ', None: ' + str(x2[x]) + ', Down: ' + str(x3[x]))
-    model_u = data.model_loader('weights_b25_150_' + str(x1[x]))
-    model_n = data.model_loader('weights_b25_150_' + str(x2[x]))
-    model_d = data.model_loader('weights_b25_150_' + str(x3[x]))
-    calculate_model(model_u, model_n, model_d, i, str(x1[x]) + ', ' + str(x2[x]) + ', ' + str(x3[x]))
-    i += 1
+    #print('>>>>>> Check models UP: ' + str(x1[x]) + ', None: ' + str(x2[x]) + ', Down: ' + str(x3[x]))
+    #model_u = data.model_loader('weights_b25_150_' + str(x1[x]))
+    #model_n = data.model_loader('weights_b25_150_' + str(x2[x]))
+    #model_d = data.model_loader('weights_b25_150_' + str(x3[x]))
+    #calculate_model(model_u, model_n, model_d, i, str(x1[x]) + ', ' + str(x2[x]) + ', ' + str(x3[x]))
+    #i += 1
 model_u = data.model_loader('weights_composite_10_135')
-model_n = data.model_loader('weights_composite_11_136')
-model_d = data.model_loader('weights_composite_12_137')
+model_n = data.model_loader('weights_composite_12_137')
+model_d = data.model_loader('weights_composite_15_140')
 calculate_model(model_u, model_n, model_d, i, '10_135_11_136_12_137')
